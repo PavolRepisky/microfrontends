b62bb874520eb6149ddcecd0886d2b8d343c4ccc
Pavol Repisky <pal.repisky@gmail.com>
2024-10-19 17:12:51 +0200

    Add support for internationalization

--- implementation/project-management-tool/projects/application-shell/src/app/app.component.ts
+++ implementation/project-management-tool/projects/application-shell/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- implementation/project-management-tool/projects/application-shell/src/app/navigation/navigation.component.ts
+++ implementation/project-management-tool/projects/application-shell/src/app/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { CommonModule, NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ThemeService } from '../theme.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 interface NavItem {
   icon: string;
@@ -13,31 +14,61 @@ interface NavItem {
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule, NgClass, RouterLink],
+  imports: [CommonModule, NgClass, RouterLink, TranslateModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
   title = 'Microfrontends';
   theme: string;
+  language: string;
 
-  constructor(private themeService: ThemeService) {
+  constructor(
+    private themeService: ThemeService,
+    private translateService: TranslateService
+  ) {
     this.theme = this.themeService.getTheme();
+    this.language =
+      localStorage.getItem('language') || this.translateService.defaultLang;
+    this.setLanguage(this.language);
+    localStorage.setItem('language', this.language);
   }
 
   navItems: NavItem[] = [
-    { icon: 'bi-house-door', label: 'Dashboard', active: true, url: '/' },
+    {
+      icon: 'bi-house-door',
+      label: 'navigation.dashboard',
+      active: true,
+      url: '/',
+    },
     {
       icon: 'bi-briefcase',
-      label: 'Projects',
+      label: 'navigation.projects',
       active: false,
       url: '/projects',
     },
-    { icon: 'bi-check-lg', label: 'Tasks', active: false, url: '/tasks' },
-    { icon: 'bi-people', label: 'Users', active: false, url: '/users' },
+    {
+      icon: 'bi-check-lg',
+      label: 'navigation.tasks',
+      active: false,
+      url: '/tasks',
+    },
+    {
+      icon: 'bi-people',
+      label: 'navigation.users',
+      active: false,
+      url: '/users',
+    },
   ];
 
-  setTheme(newTheme: string) {
-    this.themeService.setTheme(newTheme);
+  setTheme(theme: string) {
+    this.themeService.setTheme(theme);
+    this.theme = theme;
+  }
+
+  setLanguage(language: string) {
+    this.translateService.use(language);
+    this.language = language;
+    localStorage.setItem('language', language);
   }
 }
